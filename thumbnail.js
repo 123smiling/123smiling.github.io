8d42252c7438c876fd2ae9c1217690d5bf2353b6
123smiling <100742078+123smiling@users.noreply.github.com>
2022-03-13 00:05:58 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,4 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(event) {
    let targetImage = document.querySelector("#smart-image");
+   targetImage.addEventListener("click", function() {
+    alert("点击了图片");
+});
    
 });
