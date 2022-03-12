00ec52ec97a3094dde1901d1a92d7bb72f296b80
123smiling <100742078+123smiling@users.noreply.github.com>
2022-03-13 00:17:36 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,13 +1,15 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-   
+   let targetImage = document.querySelector("#smart-image");
    targetImage.addEventListener("click", function() {
-    alert("点击了图片");
-});
-   targetImage.classList.remove("small");
-if (targetImage.classList.contains("small")) {
+  if (targetImage.classList.contains("small")) {
     targetImage.classList.remove("small");
-} else {
+} 
+      else
+{
   targetImage.classList.add("small");
 }
+});
+  
+
    
 });
