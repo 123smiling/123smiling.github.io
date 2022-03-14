2530d09e2c8c6c136b3a1aeef4b4b4064df9c7e7
123smiling <100742078+123smiling@users.noreply.github.com>
2022-03-14 15:35:04 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  let targetImage = document.querySelector(".smart-image");
+  let targetImage = document.querySelector("#smart-image");
 targetImage.addEventListener("click", function() {
 if (targetImage.classList.contains("small")) 
 {
