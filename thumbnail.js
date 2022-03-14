6d69d64811f9b229ebb587872ebcbeff34cd2919
123smiling <100742078+123smiling@users.noreply.github.com>
2022-03-14 14:24:49 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,13 +1,16 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   let targetImage = document.querySelector("#smart-image");
-  let targetImage2 = document.querySelector("#smart-image2");
-  let targetImage = document.querySelector("#smart-image");
-  let targetImage = document.querySelector("#smart-image");
-  let targetImage = document.querySelector("#smart-image");
-  let targetImage = document.querySelector("#smart-image");
-  let targetImage = document.querySelector("#smart-image");
-  let targetImage = document.querySelector("#smart-image");
-  let targetImage = document.querySelector("#smart-image");
+targetImage.addEventListener("click", function() {
+if (targetImage.classList.contains("small")) 
+{
+  targetImage.classList.remove("small");
+} 
+  else
+{
+  targetImage.classList.add("small");
+}
+});
+   let targetImage = document.querySelector("#smart-image2");
 targetImage.addEventListener("click", function() {
 if (targetImage.classList.contains("small")) 
 {
@@ -18,4 +21,19 @@ if (targetImage.classList.contains("small"))
   targetImage.classList.add("small");
 }
 });     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
